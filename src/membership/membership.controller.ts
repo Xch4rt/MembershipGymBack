8260f4114a859d1603d0771d0c1532fdc3d2252f
Xch4rt <pab203.guti@gmail.com>
2023-06-03 14:50:09 -0600

    hotfix authentication

--- src/membership/membership.controller.ts
+++ src/membership/membership.controller.ts
@@ -1,32 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { MembershipService } from './membership.service';
 import { CreateMembershipDto } from './dto/create-membership.dto';
 import { UpdateMembershipDto } from './dto/update-membership.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiTags('membership')
+@ApiBearerAuth()
 @Controller('membership')
 export class MembershipController {
   constructor(private readonly membershipService: MembershipService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createMembershipDto: CreateMembershipDto) {
     return this.membershipService.create(createMembershipDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll() {
     return this.membershipService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.membershipService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMembershipDto: UpdateMembershipDto) {
     return this.membershipService.update(+id, updateMembershipDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.membershipService.remove(+id);
