86424c1806e7531ec72d20b11f62f772b24684db
Xch4rt <pab203.guti@gmail.com>
2023-11-13 12:28:47 -0600

    prisma service ref

--- src/membership/membership.controller.spec.ts
+++ src/membership/membership.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MembershipController } from './membership.controller';
 import { MembershipService } from './membership.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { EmailValidator } from 'src/utils/emailValidator';
 
 describe('MembershipController', () => {
   let controller: MembershipController;
@@ -9,7 +10,7 @@ describe('MembershipController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MembershipController],
-      providers: [MembershipService, PrismaService],
+      providers: [MembershipService, PrismaService, EmailValidator],
     }).compile();
 
     controller = module.get<MembershipController>(MembershipController);
