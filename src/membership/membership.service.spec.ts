782135fab3d590fc0a6dd592d59de1b36b6c815b
Xch4rt <pab203.guti@gmail.com>
2023-11-17 21:03:01 -0600

    fix: tests providers

--- src/membership/membership.service.spec.ts
+++ src/membership/membership.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MembershipService } from './membership.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { EmailValidator } from '../utils/emailValidator';
 
 describe('MembershipService', () => {
   let service: MembershipService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MembershipService],
+      providers: [MembershipService, PrismaService, EmailValidator],
     }).compile();
 
     service = module.get<MembershipService>(MembershipService);
